763ff4af739c4111d4df926d15f6bdbf9130743c
stanymch <59458960+stanymch@users.noreply.github.com>
2020-01-03 04:40:10 +0300

    Update Loader.java

--- Booleans/src/Loader.java
+++ Booleans/src/Loader.java
@@ -2,20 +2,24 @@
 public class Loader {
 
   public static void main(String[] args) {
-    int milkAmount = 200; // ml
-    int powderAmount = 5; // g
-    int eggsCount = 3; // items
-    int sugarAmount = 5; // g
-    int oilAmount = 30; // ml
-    int appleCount = 8; // items
+        int milkAmount = 200; // ml
+        int powderAmount = 305; // g
+        int eggsCount = 4; // items
+        int sugarAmount = 5; // g
+        int oilAmount = 30; // ml
+        int appleCount = 8;
 
-    //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-    System.out.println("Pancakes");
-
-    //milk - 300 ml, powder - 5 g, eggs - 5
-    System.out.println("Omelette");
-
-    //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-    System.out.println("Apple pie");
+        if (milkAmount >= 1000 && powderAmount >= 400 && oilAmount >= 30 && sugarAmount >= 10) {
+            //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
+            System.out.println("Pancakes");
+        }
+        if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5) {
+            //milk - 300 ml, powder - 5 g, eggs - 5
+            System.out.println("Omelette");
+        }
+        if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4) {
+            //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
+            System.out.println("Apple pie");
+        }
   }
 }
